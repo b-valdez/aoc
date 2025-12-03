951dfa16f94ea4c7f05baff609c5ad32f5b3dce4
Benjamin Valdez <benjamin.valdez+gh@gmail.com>
2025-12-03 20:30:26 +0100

    2025 Day 3 (Gloriously Functional)

--- solutions/2025/3/solution.ml
+++ solutions/2025/3/solution.ml
@@ -5,25 +5,52 @@ let parser =
   take_while1 Char.is_digit <* (end_of_line <|> end_of_input)
 ;;
 
-(* Disgustingly imperative for efficiency (premature optimisation) *)
+let iter_acc map =
+  Map.iter map |> Iter.from_labelled_iter |> Iter.map ~f:Char.get_digit_exn
+;;
+
+let iter_to_result = Iter.fold ~init:0 ~f:(fun acc digit -> (10 * acc) + digit)
+
+let insert bank n len (map, i) c : _ Continue_or_stop.t =
+  if Map.length map + len - i = n
+  then (
+    let start = iter_acc map in
+    let end_ =
+      String.slice bank i 0
+      |> String.iter
+      |> Iter.from_labelled_iter
+      |> Iter.map ~f:Char.get_digit_exn
+    in
+    let result = iter_to_result Iter.(start <+> end_) in
+    Stop result)
+  else (
+    let map =
+      Map.binary_search_subrange
+        map
+        ~compare:(fun ~key ~data bound ->
+          let num_needed_to_fill_map_from_here = n - key in
+          let num_banks_still_to_consider = len - i in
+          if num_needed_to_fill_map_from_here > num_banks_still_to_consider
+          then -1
+          else Char.descending data bound)
+        ~lower_bound:Unbounded
+        ~upper_bound:(Maybe_bound.Incl c)
+    in
+    let map =
+      if Map.length map < n then Map.add_exn map ~key:(Map.length map) ~data:c else map
+    in
+    Continue (map, i + 1))
+;;
+
 let max_joltage n bank =
   let acc = Dynarray.create () in
   Dynarray.set_capacity acc n;
   let len = String.length bank in
-  String.iteri bank ~f:(fun i c ->
-    if Dynarray.length acc + (len - i) = n
-    then Dynarray.add_last acc c
-    else
-      with_return (fun { return } ->
-        for j = max 0 (n - len + i) to Dynarray.length acc - 1 do
-          if Char.(Dynarray.get acc j < c)
-          then (
-            Dynarray.truncate acc j;
-            Dynarray.add_last acc c;
-            return ())
-        done;
-        if Dynarray.length acc < n then Dynarray.add_last acc c));
-  Dynarray.fold_left (fun joltage c -> (joltage * 10) + Char.get_digit_exn c) 0 acc
+  String.fold_until
+    bank
+    ~init:(Int.Map.empty, 0)
+    ~f:(insert bank n len)
+    ~finish:(fun (map, _) -> iter_acc map |> iter_to_result)
 ;;
 
 let part1 banks =
